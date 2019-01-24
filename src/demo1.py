0e03a25436ddd58c210032dfc8339005584402d2
xiaoye <3144605@qq.com>
2019-01-24 17:39:19 +0800

    first commit

---
+++ src/demo1.py
@@ -0,0 +1,12 @@
+
+n = 2
+m = 30
+print("n+m = ",n + m)
+
+print("Hello\
+    World")
+print("hello\nWorld")
+
+print(""" hello, \n let's go""")
+print(r"hello \n, let's go")
+print(repr("let's \n go"))
